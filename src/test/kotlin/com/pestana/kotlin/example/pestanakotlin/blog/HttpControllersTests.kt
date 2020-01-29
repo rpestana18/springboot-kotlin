3c35d1585a7b82b88821a16353b2a1d226fbe506
Rodrigo Pestana <58884259+rpestana18@users.noreply.github.com>
2020-01-28 22:20:54 -0300

    adding imports missing before

--- src/test/kotlin/com/pestana/kotlin/example/pestanakotlin/blog/HttpControllersTests.kt
+++ src/test/kotlin/com/pestana/kotlin/example/pestanakotlin/blog/HttpControllersTests.kt
@@ -5,7 +5,10 @@ import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 
 @WebMvcTest
 class HttpControllersTests(@Autowired val mockMvc: MockMvc) {
